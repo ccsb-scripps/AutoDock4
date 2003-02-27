f3d9cc96a0c56719df607a101382c65ffccc05cb
William 'Lindy' Lindstrom <lindy@scripps.edu>
2003-02-27 02:11:08 +0000

    Removed $Log$ cvs keyword. Here's what it said:
    /***** RCS INFO **********************************************************
    
    $Id: gencau.cc,v 1.2 2003/02/26 01:02:47 garrett Exp $
    $Source: /export/cvs/autodock/dist305/src/autodock/gencau.cc,v $
    $Log: gencau.cc,v $
    Revision 1.2  2003/02/26 01:02:47  garrett
    
    General changes to generate identical output on each hardware/os
    platform (as much as possible):
    
        -DUSE_DOUBLE compile switch introduced. Mixed float/double
        precision would be used otherwise, as before the "pre-integration-305"
        stage.  This switch controls the new typedef "FloatOrDouble", which is
        "double" when -DUSE_DOUBLE is used as a compile-time flag, otherwise this
        is typedef'ed to "float". (GMM, WML)
    
        Prior to switching all floats to doubles, the AutoDock docking
        log files often had differences in energies, coordinates or state
        variables in the sixth (or so) decimal place of floats.
    
    Lindy added the HAVE_CONFIG_H compile-time switch with "config.h"
    in preparation for "autoconf" and "configure"... (WML)
    
    Revision 1.1.1.1  2001/08/13 22:05:53  gillet
     import initial of autodock sources
    
    // Revision 3.0  1996/03/11  05:40:00  halliday
    // The function definition for the GA/LS hybrid.
    //
    
    ****** RCS INFO *********************************************************/
    
    CVS-ID: gencau.cc 1.3

--- gencau.cc
+++ gencau.cc
@@ -1,6 +1,6 @@
 /*
 
- $Id: gencau.cc,v 1.2 2003/02/26 01:02:47 garrett Exp $
+ $Id: gencau.cc,v 1.3 2003/02/27 02:11:08 lindy Exp $
 
 */
 
@@ -8,41 +8,6 @@
 #include <config.h>
 #endif
 
-/***** RCS INFO **********************************************************
-
-$Id: gencau.cc,v 1.2 2003/02/26 01:02:47 garrett Exp $
-$Source: /Users/mp/facil/autodock/git-luna/autodock-cvstar/gencau.cc,v $
-$Log: gencau.cc,v $
-Revision 1.2  2003/02/26 01:02:47  garrett
-
-General changes to generate identical output on each hardware/os
-platform (as much as possible):
-
-    -DUSE_DOUBLE compile switch introduced. Mixed float/double
-    precision would be used otherwise, as before the "pre-integration-305"
-    stage.  This switch controls the new typedef "FloatOrDouble", which is
-    "double" when -DUSE_DOUBLE is used as a compile-time flag, otherwise this
-    is typedef'ed to "float". (GMM, WML)
-
-    Prior to switching all floats to doubles, the AutoDock docking
-    log files often had differences in energies, coordinates or state
-    variables in the sixth (or so) decimal place of floats.
-
-Lindy added the HAVE_CONFIG_H compile-time switch with "config.h"
-in preparation for "autoconf" and "configure"... (WML)
-
-Revision 1.1.1.1  2001/08/13 22:05:53  gillet
- import initial of autodock sources
-
-// Revision 3.0  1996/03/11  05:40:00  halliday
-// The function definition for the GA/LS hybrid.
-//
-
-****** RCS INFO *********************************************************/
-
-/* gencau.c
- *
- */
 
 #include <math.h>
 #include "ranlib.h"
