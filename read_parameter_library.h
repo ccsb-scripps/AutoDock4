424b574ba0878dd011061fc8a36629478e79f9c4
Garrett Matthew Morris <garrett@scripps.edu>
2005-08-16 00:06:48 +0000

    Used in reading in the new AutoDock 4 parameter library files.
    CVS-ID: read_parameter_library.cc 1.1
    CVS-ID: read_parameter_library.h 1.1

---
+++ read_parameter_library.h
@@ -0,0 +1,14 @@
+#ifndef _READ_PARAMETER_LIBRARY
+#define _READ_PARAMETER_LIBRARY
+
+#include "autocomm.h"
+
+void read_parameter_library(
+        char FN_parameter_library[MAX_CHARS],
+        int outlev
+        );
+
+void setup_parameter_library(
+        int outlev
+        );
+#endif
