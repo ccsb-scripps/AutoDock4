ccff2ee28dee7ed9836839a5957f218661eff519
Rick Alther <alther@us.ibm.com>
2005-10-10 16:57:26 +0000

    * Changes for port to Win32
    CVS-ID: timesys.h 1.1.1.1.8.1

--- timesys.h
+++ timesys.h
@@ -3,7 +3,13 @@
 #ifndef TIMESYS
 #define TIMESYS
 #include <sys/types.h>
+
+#ifdef _WIN32
+#include "times.h"
+#else
 #include <sys/times.h>
+#endif
+
 #include <time.h>
 #include "autocomm.h"
 void  timesys( Clock  duration,
