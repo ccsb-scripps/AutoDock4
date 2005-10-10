56286b12b5df5e7523a150dd80fe0191daaaa653
Rick Alther <alther@us.ibm.com>
2005-10-10 16:50:47 +0000

    * Added header guard * Updated #includes.
    CVS-ID: printdate.h 1.1.1.1.8.1

--- printdate.h
+++ printdate.h
@@ -1,6 +1,11 @@
+/*
+ $Id: printdate.h,v 1.1.1.1.8.1 2005/10/10 16:50:47 alther Exp $
+*/
 
 #ifndef PRINTDATE
 #define PRINTDATE
-#include "autocomm.h"
+
+#include <stdio.h>
 void  printdate( FILE  *fp, int   flag );
+
 #endif
