ac37a25a3cd59e621b98a2789f2fb5664969158d
Rick Alther <alther@us.ibm.com>
2005-10-11 00:12:16 +0000

    * Changes for Win32 port.
    CVS-ID: output_state.cc 1.3.8.1

--- output_state.cc
+++ output_state.cc
@@ -1,6 +1,6 @@
 /*
 
- $Id: output_state.cc,v 1.3 2004/02/12 04:32:15 garrett Exp $
+ $Id: output_state.cc,v 1.3.8.1 2005/10/11 00:12:16 alther Exp $
 
 */
 
@@ -10,19 +10,32 @@
 
 /* output_state.cc */
 
-    #include <stdio.h>
-    #include <fcntl.h>
-    #include <unistd.h>
-    #include <sys/stat.h>
-    #include <sys/types.h>
-    #include "structs.h"
-    #include "output_state.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+#ifdef _WIN32
+   #include <io.h>
+   #include <sys/locking.h>
+#else
+   #include <unistd.h>
+#endif
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "structs.h"
+#include "output_state.h"
 
 /* LOCK_SH 1       shared lock */
 /* LOCK_EX 2       exclusive lock */
 /* LOCK_NB 4       don't block when locking */
 /* LOCK_UN 8       unlock */
-#define PERMS 0666        /* hexadecimal permissions for watch-file */
+
+/* hexadecimal permissions for watch-file */
+#ifdef _WIN32
+   #define PERMS (_S_IREAD | _S_IWRITE)
+#else
+   #define PERMS 0666
+#endif
 
 /*----------------------------------------------------------------------------*/
 void output_state( FILE *fp,
@@ -60,17 +73,26 @@ void output_state( FILE *fp,
 #ifndef __ppc__
     if (B_watch) {
 
-        if ((FD_watch = creat( FN_watch, PERMS )) != -1) {;
-            /* creates new file, or re-write old one */
+       /* creates new file, or re-write old one */
+#ifdef _WIN32
+       if ((FD_watch = _creat( FN_watch, PERMS )) != -1) {
+#else
+       if ((FD_watch = creat( FN_watch, PERMS )) != -1) {
+#endif
 
             if ((FP_watch = fdopen( FD_watch, "w")) != NULL ) {
                 /*lockf_status = lockf( FD_watch, F_LOCK, 0 ); */
-                (void) lockf( FD_watch, F_LOCK, 0 ); 
+#ifdef _WIN32
+                (void) _locking(FD_watch, _LK_LOCK, 0);
+#else
+                (void) lockf( FD_watch, F_LOCK, 0 );
+#endif
 
                 for (i = 0;  i < natom;  i++) {
-		    fprintf( FP_watch, "%30s%8.3f%8.3f%8.3f\n",
-		    atomstuff[i], crd[i][X], crd[i][Y], crd[i][Z]);
+		             fprintf( FP_watch, "%30s%8.3f%8.3f%8.3f\n",
+		                       atomstuff[i], crd[i][X], crd[i][Y], crd[i][Z]);
                 }
+
                 fclose( FP_watch ); /*lockf_status=lockf(FD_watch,F_ULOCK,0);*/
             }
         }
