904d6d99ec19848c88516649ff7ae5aedaca4c5b
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-12 03:43:38 +0000

    Introducing code to parse the new PDBQT format.
    CVS-ID: parse_PDBQT_line.cc 1.1
    CVS-ID: parse_PDBQT_line.h 1.1

---
+++ parse_PDBQT_line.h
@@ -0,0 +1,10 @@
+#ifndef PARSE_PDBQT_LINE
+#define PARSE_PDBQT_LINE
+
+#include "constants.h"
+#include "strindex.h"
+#include "PDBQT_tokens.h"
+
+int  parse_PDBQT_line( char  line[LINE_LEN] );
+
+#endif
