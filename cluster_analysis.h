f9018b3d6e4f1ed6b1109aa4e1d85dbc686c225f
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:31:56 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: cluster_analysis.h 1.3

--- cluster_analysis.h
+++ cluster_analysis.h
@@ -3,19 +3,19 @@
 #define CLUSTER_ANALYSIS
 #include "constants.h"
 #include "getrms.h"
-int  cluster_analysis( FloatOrDouble clus_rms_tol, 
+int  cluster_analysis( Real clus_rms_tol, 
                        int   cluster[MAX_RUNS][MAX_RUNS], 
                        int   num_in_clus[MAX_RUNS], 
                        int   isort[MAX_RUNS], 
                        int   nconf, 
                        int   natom, 
                        int   type[MAX_ATOMS], 
-                       FloatOrDouble crd[MAX_RUNS][MAX_ATOMS][SPACE], 
-                       FloatOrDouble crdpdb[MAX_ATOMS][SPACE], 
-                       FloatOrDouble sml_center[SPACE], 
-                       FloatOrDouble clu_rms[MAX_RUNS][MAX_RUNS], 
+                       Real crd[MAX_RUNS][MAX_ATOMS][SPACE], 
+                       Real crdpdb[MAX_ATOMS][SPACE], 
+                       Real sml_center[SPACE], 
+                       Real clu_rms[MAX_RUNS][MAX_RUNS], 
                        Boole B_symmetry_flag,
-                       FloatOrDouble ref_crds[MAX_ATOMS][SPACE],
+                       Real ref_crds[MAX_ATOMS][SPACE],
                        int   ref_natoms,
-                       FloatOrDouble ref_rms[MAX_RUNS]);
+                       Real ref_rms[MAX_RUNS]);
 #endif
