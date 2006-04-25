6d57f34b924548b94833c7e99f99da327c073104
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:33:22 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: summarizegrids.h 1.3

--- summarizegrids.h
+++ summarizegrids.h
@@ -3,8 +3,8 @@
 #define SUMMARIZEGRIDS
 #include "constants.h"
 void  summarizegrids( char  atm_tyP_str[ATOM_MAPS],
-                      FloatOrDouble mapmax[MAX_MAPS],
-                      FloatOrDouble mapmin[MAX_MAPS],
+                      Real mapmax[MAX_MAPS],
+                      Real mapmin[MAX_MAPS],
                       int   num_all_maps,
                       int   num_atm_maps );
 #endif
