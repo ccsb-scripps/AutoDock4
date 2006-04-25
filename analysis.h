adfaf13fe86bd6317ad962d3ba997a1bc5341240
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:31:42 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: analysis.h 1.10

--- analysis.h
+++ analysis.h
@@ -17,18 +17,18 @@
 
 void  analysis( int   Nnb, 
                 char  atomstuff[MAX_ATOMS][MAX_CHARS], 
-                FloatOrDouble charge[MAX_ATOMS], 
-                FloatOrDouble abs_charge[MAX_ATOMS], 
-                FloatOrDouble qsp_abs_charge[MAX_ATOMS], 
+                Real charge[MAX_ATOMS], 
+                Real abs_charge[MAX_ATOMS], 
+                Real qsp_abs_charge[MAX_ATOMS], 
 	            Boole B_calcIntElec,
-	            FloatOrDouble q1q2[MAX_NONBONDS],
-                FloatOrDouble clus_rms_tol, 
-                FloatOrDouble crdpdb[MAX_ATOMS][SPACE], 
+	            Real q1q2[MAX_NONBONDS],
+                Real clus_rms_tol, 
+                Real crdpdb[MAX_ATOMS][SPACE], 
                 
                 EnergyTables *ptr_ad_energy_tables,
 
-                FloatOrDouble map[MAX_GRID_PTS][MAX_GRID_PTS][MAX_GRID_PTS][MAX_MAPS], 
-                FloatOrDouble econf[MAX_RUNS], 
+                Real map[MAX_GRID_PTS][MAX_GRID_PTS][MAX_GRID_PTS][MAX_MAPS], 
+                Real econf[MAX_RUNS], 
                 int   irunmax, 
                 int   natom, 
                 int   **nonbondlist, 
@@ -36,25 +36,25 @@ void  analysis( int   Nnb,
                 int   ntor, 
                 State hist[MAX_RUNS], 
                 char  smFileName[MAX_CHARS], 
-                FloatOrDouble sml_center[SPACE], 
+                Real sml_center[SPACE], 
                 Boole B_symmetry_flag, 
                 int   tlist[MAX_TORS][MAX_ATOMS], 
                 int   type[MAX_ATOMS], 
-                FloatOrDouble vt[MAX_TORS][SPACE],
+                Real vt[MAX_TORS][SPACE],
 		        char  rms_ref_crds[MAX_CHARS],
-		        FloatOrDouble torsFreeEnergy,
+		        Real torsFreeEnergy,
                 Boole B_write_all_clusmem,
                 int ligand_is_inhibitor,
                 Boole B_template,
-                FloatOrDouble template_energy[MAX_ATOMS], // template energy value for each atom
-                FloatOrDouble template_stddev[MAX_ATOMS], // and standard deviation of this energy
+                Real template_energy[MAX_ATOMS], // template energy value for each atom
+                Real template_stddev[MAX_ATOMS], // and standard deviation of this energy
                 int   outlev,
                 int   ignore_inter[MAX_ATOMS],
                 const Boole   B_include_1_4_interactions,
-                const FloatOrDouble scale_1_4,
+                const Real scale_1_4,
 
                 const ParameterEntry parameterArray[MAX_MAPS],
-                const FloatOrDouble unbound_internal_FE,
+                const Real unbound_internal_FE,
                 
                 GridMapSetInfo *info);
 #endif
