363a1d4aad7f922fe520d63deb5968a415701f7b
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:32:10 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: evaluate_energy.h 1.8

--- evaluate_energy.h
+++ evaluate_energy.h
@@ -6,20 +6,20 @@
 #include "trilinterp.h"
 #include "eintcal.h"
 
-FloatOrDouble evaluate_energy( 
-    FloatOrDouble crd[MAX_ATOMS][SPACE],
-    FloatOrDouble charge[MAX_ATOMS],
-    FloatOrDouble abs_charge[MAX_ATOMS],
+Real evaluate_energy( 
+    Real crd[MAX_ATOMS][SPACE],
+    Real charge[MAX_ATOMS],
+    Real abs_charge[MAX_ATOMS],
     int   type[MAX_ATOMS],
     int   natom,
-    FloatOrDouble map[MAX_GRID_PTS][MAX_GRID_PTS][MAX_GRID_PTS][MAX_MAPS],
+    Real map[MAX_GRID_PTS][MAX_GRID_PTS][MAX_GRID_PTS][MAX_MAPS],
     int   **nonbondlist,
 
     EnergyTables *ptr_ad_energy_tables,
     
     int   Nnb,
     Boole B_calcIntElec,
-    FloatOrDouble q1q2[MAX_NONBONDS],
+    Real q1q2[MAX_NONBONDS],
     Boole B_isGaussTorCon,
     Boole B_isTorConstrained[MAX_TORS],
     State now,
@@ -27,9 +27,9 @@ FloatOrDouble evaluate_energy(
     unsigned short US_TorE[MAX_TORS],
     unsigned short US_torProfile[MAX_TORS][NTORDIVS],
     const Boole         B_include_1_4_interactions,
-    const FloatOrDouble scale_1_4,
+    const Real scale_1_4,
     const ParameterEntry parameterArray[MAX_MAPS],
-    const FloatOrDouble unbound_internal_FE,
+    const Real unbound_internal_FE,
 
     GridMapSetInfo *info
     );
