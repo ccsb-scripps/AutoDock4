87eb3d8145ed4d9fc64b05a60a4b0b1f4596e5fa
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:33:29 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: torsion.h 1.3

--- torsion.h
+++ torsion.h
@@ -2,8 +2,8 @@
 #define TORSION
 
 void  torsion( State now,
-               FloatOrDouble crd[MAX_ATOMS][SPACE], 
-	       FloatOrDouble vt[MAX_TORS][SPACE], 
+               Real crd[MAX_ATOMS][SPACE], 
+	       Real vt[MAX_TORS][SPACE], 
                int   tlist[MAX_TORS][MAX_ATOMS], 
                int   ntor );
 #endif
