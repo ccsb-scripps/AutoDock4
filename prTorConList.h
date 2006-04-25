81b3d560186a4aad60185f41d6f489755ec10656
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:32:49 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: prTorConList.h 1.3

--- prTorConList.h
+++ prTorConList.h
@@ -5,6 +5,6 @@
 void  prTorConList( int   ntor,
 		    Boole B_isTorConstrained[MAX_TORS],
 		    unsigned short US_torProfile[MAX_TORS][NTORDIVS],
-		    FloatOrDouble F_TorConRange[MAX_TORS][MAX_TOR_CON][2],
+		    Real F_TorConRange[MAX_TORS][MAX_TOR_CON][2],
 		    int   N_con[MAX_TORS]);
 #endif
