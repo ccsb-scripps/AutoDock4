a52f13ce17ed08d38c9731857a20e7f252df2db8
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:33:31 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: trilinterp.h 1.7

--- trilinterp.h
+++ trilinterp.h
@@ -8,16 +8,16 @@
 #define ALL_ATOMS_INSIDE_GRID 0
 #define SOME_ATOMS_OUTSIDE_GRID 1
 
-#define NULL_EVDW ((FloatOrDouble *)NULL)
-#define NULL_ELEC ((FloatOrDouble *)NULL)
-#define NULL_EVDW_TOTAL ((FloatOrDouble *)NULL)
-#define NULL_ELEC_TOTAL ((FloatOrDouble *)NULL)
+#define NULL_EVDW ((Real *)NULL)
+#define NULL_ELEC ((Real *)NULL)
+#define NULL_EVDW_TOTAL ((Real *)NULL)
+#define NULL_ELEC_TOTAL ((Real *)NULL)
 #define NULL_IGNORE_INTERMOL ((int *)NULL)
 
 #include "constants.h"
 #include "structs.h"
 
-FloatOrDouble  trilinterp( CONST_FLOAT tcoord[MAX_ATOMS][SPACE], // temporary coordinates
+Real  trilinterp( CONST_FLOAT tcoord[MAX_ATOMS][SPACE], // temporary coordinates
  CONST_FLOAT charge[MAX_ATOMS], // partial atomic charges
  CONST_FLOAT abs_charge[MAX_ATOMS], 
  CONST_INT   type[MAX_ATOMS], // atom type of each atom
@@ -26,13 +26,13 @@ FloatOrDouble  trilinterp( CONST_FLOAT tcoord[MAX_ATOMS][SPACE], // temporary co
  GridMapSetInfo *info, // info->lo[X],info->lo[Y],info->lo[Z],    minimum coordinates in x,y,z
  int some_atoms_outside_grid, // boolean
  int ignore_inter[MAX_ATOMS], // array of booleans, says to ignore computation intermolecular energies per atom
- FloatOrDouble elec[MAX_ATOMS], // set if not NULL - electrostatic energies, atom by atom
- FloatOrDouble emap[MAX_ATOMS],  // set if not NULL - intermolecular energies
- FloatOrDouble *p_elec_total, // set if not NULL - total electrostatic energy
- FloatOrDouble *p_emap_total // set if not NULL - total intermolecular energy
+ Real elec[MAX_ATOMS], // set if not NULL - electrostatic energies, atom by atom
+ Real emap[MAX_ATOMS],  // set if not NULL - intermolecular energies
+ Real *p_elec_total, // set if not NULL - total electrostatic energy
+ Real *p_emap_total // set if not NULL - total intermolecular energy
  );
 
-FloatOrDouble  template_trilinterp( CONST_FLOAT tcoord[MAX_ATOMS][SPACE], // temporary coordinates
+Real  template_trilinterp( CONST_FLOAT tcoord[MAX_ATOMS][SPACE], // temporary coordinates
  CONST_FLOAT charge[MAX_ATOMS], // partial atomic charges
  CONST_FLOAT abs_charge[MAX_ATOMS], 
  CONST_INT   type[MAX_ATOMS], // atom type of each atom
@@ -43,10 +43,10 @@ FloatOrDouble  template_trilinterp( CONST_FLOAT tcoord[MAX_ATOMS][SPACE], // tem
  int ignore_inter[MAX_ATOMS], // array of booleans, says to ignore computation intermolecular energies per atom
  CONST_FLOAT template_energy[MAX_ATOMS],
  CONST_FLOAT template_stddev[MAX_ATOMS],
- FloatOrDouble elec[MAX_ATOMS], // set if not NULL - electrostatic energies, atom by atom
- FloatOrDouble emap[MAX_ATOMS],  // set if not NULL - intermolecular energies
- FloatOrDouble *p_elec_total, // set if not NULL - total electrostatic energy
- FloatOrDouble *p_emap_total // set if not NULL - total intermolecular energy
+ Real elec[MAX_ATOMS], // set if not NULL - electrostatic energies, atom by atom
+ Real emap[MAX_ATOMS],  // set if not NULL - intermolecular energies
+ Real *p_elec_total, // set if not NULL - total electrostatic energy
+ Real *p_emap_total // set if not NULL - total intermolecular energy
  );
 #endif
 
