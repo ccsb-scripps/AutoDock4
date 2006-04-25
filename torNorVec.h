8fe8073f92c401a0a4d9791886a2d31b7f903735
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:33:27 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: torNorVec.h 1.3

--- torNorVec.h
+++ torNorVec.h
@@ -3,8 +3,8 @@
 #define TORNORVEC
 #include "constants.h"
 #include "stop.h"
-void  torNorVec( FloatOrDouble crdpdb[MAX_ATOMS][SPACE],
+void  torNorVec( Real crdpdb[MAX_ATOMS][SPACE],
                  int   ntor,
                  int   tlist[MAX_TORS][MAX_ATOMS],
-                 FloatOrDouble vt[MAX_TORS][SPACE] );
+                 Real vt[MAX_TORS][SPACE] );
 #endif
