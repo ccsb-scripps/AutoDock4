10d5b4d7ea0f450da81aa70d2d82e61fdb9bd63e
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:32:06 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: eintcalPrint.h 1.8

--- eintcalPrint.h
+++ eintcalPrint.h
@@ -2,12 +2,12 @@
 #ifndef EINTCALPRINT
 #define EINTCALPRINT
 #include "constants.h"
-FloatOrDouble  eintcalPrint( int   **nonbondlist, 
+Real  eintcalPrint( int   **nonbondlist, 
                              EnergyTables *ptr_ad_energy_tables,
-                             FloatOrDouble tcoord[MAX_ATOMS][SPACE], 
+                             Real tcoord[MAX_ATOMS][SPACE], 
                              int   atmtyp[MAX_ATOMS], 
                              int   Nnb,
                              Boole B_calcIntElec,
-                             FloatOrDouble q1q2[MAX_NONBONDS],
-                             FloatOrDouble abs_charge[MAX_ATOMS]);
+                             Real q1q2[MAX_NONBONDS],
+                             Real abs_charge[MAX_ATOMS]);
 #endif
