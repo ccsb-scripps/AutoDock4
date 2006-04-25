aeb8f702d5fbebd2fe41347e74ea87e965a2ae1c
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:32:35 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: mkRandomState.h 1.4

--- mkRandomState.h
+++ mkRandomState.h
@@ -7,7 +7,7 @@
 #include "constants.h"
 
 State mkRandomState( int   ntor,
-		     FloatOrDouble F_TorConRange[MAX_TORS][MAX_TOR_CON][2],
+		     Real F_TorConRange[MAX_TORS][MAX_TOR_CON][2],
 		     int   N_con[MAX_TORS],
                      GridMapSetInfo *info);
 #endif
