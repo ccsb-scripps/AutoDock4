46191d1878d94f08e9ec10ed90aa04bb6f9fd049
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:32:13 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: getInitialState.h 1.9

--- getInitialState.h
+++ getInitialState.h
@@ -12,8 +12,8 @@
 #include "timesys.h"
 
 void getInitialState(  
-            FloatOrDouble *Addr_e0,
-            FloatOrDouble e0max,
+            Real *Addr_e0,
+            Real e0max,
 
 	    State *sInit,
 	    State *sMin,
@@ -23,43 +23,43 @@ void getInitialState(
             Boole B_RandomQuat0,
             Boole B_RandomDihe0,
 
-            FloatOrDouble charge[MAX_ATOMS],
-            FloatOrDouble abs_charge[MAX_ATOMS],
-            FloatOrDouble qsp_abs_charge[MAX_ATOMS],
-            FloatOrDouble q1q2[MAX_NONBONDS],
-            FloatOrDouble crd[MAX_ATOMS][SPACE],
-            FloatOrDouble crdpdb[MAX_ATOMS][SPACE],
+            Real charge[MAX_ATOMS],
+            Real abs_charge[MAX_ATOMS],
+            Real qsp_abs_charge[MAX_ATOMS],
+            Real q1q2[MAX_NONBONDS],
+            Real crd[MAX_ATOMS][SPACE],
+            Real crdpdb[MAX_ATOMS][SPACE],
             char  atomstuff[MAX_ATOMS][MAX_CHARS],
-            FloatOrDouble elec[MAX_ATOMS],
-            FloatOrDouble emap[MAX_ATOMS],
+            Real elec[MAX_ATOMS],
+            Real emap[MAX_ATOMS],
 
             EnergyTables *ptr_ad_energy_tables,
 
             Boole B_calcIntElec,
-            FloatOrDouble map[MAX_GRID_PTS][MAX_GRID_PTS][MAX_GRID_PTS][MAX_MAPS],
+            Real map[MAX_GRID_PTS][MAX_GRID_PTS][MAX_GRID_PTS][MAX_MAPS],
             int   natom,
             int   Nnb,
             int   **nonbondlist,
             int   ntor,
             int   tlist[MAX_TORS][MAX_ATOMS],
             int   type[MAX_ATOMS],
-            FloatOrDouble vt[MAX_TORS][SPACE],
+            Real vt[MAX_TORS][SPACE],
             int   irun1,
             int   outlev,
 	        int   MaxRetries,
 
-	        FloatOrDouble torsFreeEnergy,
+	        Real torsFreeEnergy,
 
             int   ligand_is_inhibitor,
 
             int ignore_inter[MAX_ATOMS],
 
             const Boole         B_include_1_4_interactions,
-            const FloatOrDouble scale_1_4,
+            const Real scale_1_4,
 
             const ParameterEntry parameterArray[MAX_MAPS],
 
-            const FloatOrDouble unbound_internal_FE,
+            const Real unbound_internal_FE,
 
             GridMapSetInfo *info
            );
