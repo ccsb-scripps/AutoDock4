c657c1d2dc16db89947b9fe6d633f8fc12f473fb
Garrett Matthew Morris <garrett@scripps.edu>
2006-04-25 22:31:50 +0000

    "FloatOrDouble" is now "Real";  added support for reading input flexible-residues PDBQT files.
    CVS-ID: changeState.h 1.3

--- changeState.h
+++ changeState.h
@@ -2,9 +2,9 @@
 #define CHANGESTATE
 
 State  changeState( State last,      /* ...must be a normalized quaternion! */
-		    FloatOrDouble trnStep,
-		    FloatOrDouble torStep,
+		    Real trnStep,
+		    Real torStep,
 		    int   ntor,
-		    FloatOrDouble F_TorConRange[MAX_TORS][MAX_TOR_CON][2],
+		    Real F_TorConRange[MAX_TORS][MAX_TOR_CON][2],
 		    int   N_con[MAX_TORS]);
 #endif
