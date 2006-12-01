8c9b5042fbddb65631780dfc248b0f61515208b2
Garrett Matthew Morris <garrett@scripps.edu>
2006-12-01 02:24:44 +0000

    *** empty log message ***
    CVS-ID: qmultiply.h 1.3

--- qmultiply.h
+++ qmultiply.h
@@ -13,4 +13,7 @@ void printQuat( FILE *fp, Quat q );
 Quat uniformQuat( void );
 void unitQuat2rotation( Quat *q );
 Quat convertQuatToRot( Quat q );
+Quat convertRotToQuat( Quat q );
+Quat normQuat( Quat q );
+Quat normRot( Quat q );
 #endif
