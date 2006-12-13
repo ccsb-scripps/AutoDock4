ba8fbfedc01ee7f853f13a917e71db94020bd40f
Garrett Matthew Morris <garrett@scripps.edu>
2006-12-13 03:17:08 +0000

    *** empty log message ***
    CVS-ID: weedbonds.h 1.8

--- weedbonds.h
+++ weedbonds.h
@@ -3,6 +3,7 @@
 
 #include "constants.h"
 #include "stop.h"
+#include "structs.h"
 
 void  weedbonds( int   natom,
                  char  pdbaname[MAX_ATOMS][5],
@@ -11,7 +12,7 @@ void  weedbonds( int   natom,
                  int   tlist[MAX_TORS][MAX_ATOMS],
                  int   nbmatrix_binary[MAX_ATOMS][MAX_ATOMS],
                  int   *P_Nnb,
-                 int   **nonbondlist,
+                 NonbondParam *nonbondlist,
                  int   outlev,
 				 int   type[MAX_ATOMS]);
 #endif
@@ -22,6 +23,7 @@ void  weedbonds( int   natom,
 
 #include "constants.h"
 #include "stop.h"
+#include "structs.h"
 
 void print_nonbonds(
                 int natom,
@@ -31,7 +33,7 @@ void print_nonbonds(
                 int tlist[MAX_TORS][MAX_ATOMS],
                 int nbmatrix[MAX_ATOMS][MAX_ATOMS],
                 int Nnb,
-                int **nonbondlist,
+                NonbondParam *nonbondlist,
                 int outlev,
                 int type[MAX_ATOMS]);
 #endif
