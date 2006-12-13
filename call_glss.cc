c02fddf7d7c06a4fbaeced3a5fcf69fb1fe5eeb2
Garrett Matthew Morris <garrett@scripps.edu>
2006-12-13 00:10:53 +0000

    *** empty log message ***
    CVS-ID: call_glss.cc 1.24

--- call_glss.cc
+++ call_glss.cc
@@ -1,6 +1,6 @@
 /*
 
- $Id: call_glss.cc,v 1.23 2006/12/01 01:18:28 garrett Exp $
+ $Id: call_glss.cc,v 1.24 2006/12/13 00:10:53 garrett Exp $
 
 */
 
@@ -202,34 +202,34 @@ State call_glss(Global_Search *global_method, Local_Search *local_method,
             /// gmm 2006-10-18 thisPop[i].phenotyp = thisPop[i].mapping();
         }
 
-        // If initial values were supplied, put them in thisPop[0]
-        if (!B_RandomTran0) {
+    // If initial values were supplied, put them in thisPop[0]
+    if (!B_RandomTran0) {
               if (outlev > 1) { (void)fprintf(logFile, "Setting the initial translation (tran0) for individual number %d to %.2lf %.2lf %.2lf\n\n", indiv+1, sInit.T.x, sInit.T.y, sInit.T.z); }
           thisPop[indiv].genotyp.write( sInit.T.x, 0);
           thisPop[indiv].genotyp.write( sInit.T.y, 1);
           thisPop[indiv].genotyp.write( sInit.T.z, 2);
-          // Remember to keep the phenotype up-to-date
-          thisPop[indiv].phenotyp = thisPop[indiv].mapping();
+      // Remember to keep the phenotype up-to-date
+      thisPop[indiv].phenotyp = thisPop[indiv].mapping();
         }
-        if (!B_RandomQuat0) {
+    if (!B_RandomQuat0) {
               if (outlev > 1) { (void)fprintf(logFile, "Setting the initial quaternion (quat0) for individual number %d to %.2lf %.2lf %.2lf  %.2lf deg\n\n", indiv+1, sInit.Q.nx, sInit.Q.ny, sInit.Q.nz, RadiansToDegrees(sInit.Q.ang)); }
           thisPop[indiv].genotyp.write( sInit.Q.nx, 3);
           thisPop[indiv].genotyp.write( sInit.Q.ny, 4);
           thisPop[indiv].genotyp.write( sInit.Q.nz, 5);
           thisPop[indiv].genotyp.write( sInit.Q.ang, 6);
-          // Remember to keep the phenotype up-to-date
-          thisPop[indiv].phenotyp = thisPop[indiv].mapping();
+      // Remember to keep the phenotype up-to-date
+      thisPop[indiv].phenotyp = thisPop[indiv].mapping();
         }
         if (sInit.ntor > 0) {
-            if (!B_RandomDihe0) {
-                  if (outlev > 1) { (void)fprintf(logFile, "Setting the initial torsions (dihe0) for individual number %d to ", indiv+1); }
+    if (!B_RandomDihe0) {
+          if (outlev > 1) { (void)fprintf(logFile, "Setting the initial torsions (dihe0) for individual number %d to ", indiv+1); }
                     for (j=0; j<sInit.ntor; j++) {
                         thisPop[indiv].genotyp.write( sInit.tor[j], 7+j);
                         if (outlev > 1) { (void)fprintf(logFile, "%.2lf ", RadiansToDegrees(sInit.tor[j])); }
-                    };
-                  if (outlev > 1) { (void)fprintf(logFile, " deg\n\n"); }
-              // Remember to keep the phenotype up-to-date
-              thisPop[indiv].phenotyp = thisPop[indiv].mapping();
+            };
+          if (outlev > 1) { (void)fprintf(logFile, " deg\n\n"); }
+      // Remember to keep the phenotype up-to-date
+      thisPop[indiv].phenotyp = thisPop[indiv].mapping();
             }
         }
 
