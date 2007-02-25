01b786686f13443c8390d61948a88fb376e2a2ee
Garrett Matthew Morris <garrett@scripps.edu>
2007-02-25 05:47:24 +0000

    Introduced the update_torsion_vectors() function, which prints a message saying it is "Calculating unit vectors for each torsion", then calls "torNorVec()" and updates the Molecule,s vt and tlist arrays.
    CVS-ID: torNorVec.h 1.4

--- torNorVec.h
+++ torNorVec.h
@@ -3,8 +3,18 @@
 #define TORNORVEC
 #include "constants.h"
 #include "stop.h"
+#include "structs.h"
+
 void  torNorVec( Real crdpdb[MAX_ATOMS][SPACE],
                  int   ntor,
                  int   tlist[MAX_TORS][MAX_ATOMS],
                  Real vt[MAX_TORS][SPACE] );
+
+void update_torsion_vectors( Real crdpdb[MAX_ATOMS][SPACE],
+                             int ntor,
+                             int  tlist[MAX_TORS][MAX_ATOMS],
+                             Real vt[MAX_TORS][SPACE],
+                             Molecule *ligand,
+                             int debug );
+
 #endif
