199456259fc37ae04a1b00942c451664e8228b39
Garrett Matthew Morris <garrett@scripps.edu>
2007-03-14 01:37:34 +0000

    No longer used.

--- writeMolAsPDBQ.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WRITEMOLASPDBQ
-#define _WRITEMOLASPDBQ
-
-extern FILE *stateFile;
-extern int write_stateFile;
-
-void writeMolAsPDBQ(Molecule *mol, FILE *output);
-
-#endif /* WRITEPDBQSTATE */
