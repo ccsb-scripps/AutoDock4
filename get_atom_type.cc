2648f38d379473c43cdd054c4f292d39a26feab4
Ruth Huey <rhuey@scripps.edu>
2009-02-24 01:32:34 +0000

    allocated storage for message variable
    CVS-ID: get_atom_type.cc 1.7

--- get_atom_type.cc
+++ get_atom_type.cc
@@ -1,6 +1,6 @@
 /*
 
- $Id: get_atom_type.cc,v 1.6 2008/11/08 00:37:22 rhuey Exp $
+ $Id: get_atom_type.cc,v 1.7 2009/02/24 01:32:34 rhuey Exp $
 
  AutoDock 
 
@@ -50,7 +50,7 @@ int get_atom_type( char aname[4] )
     ParameterEntry thisparm;
     int map_index = -1;
     int bond_index = -1;
-    char *message;
+    char message[512];
 
     // "map_index" is used as an index into the "map" array,
     // to look up the correct energies in the current grid cell,
