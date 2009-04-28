f5f0d3644d464ffdca17969d4c862515eef19f91
Ruth Huey <rhuey@scripps.edu>
2009-04-28 21:12:42 +0000

    Changed so now Individual does mapping of its genotype into its phenotype and inverse_mapping of its phenotype into its genotype; in both cases returns a reference to itself; added a check for self-assignment
    CVS-ID: mapping.cc 1.4

--- mapping.cc
+++ mapping.cc
@@ -1,6 +1,6 @@
 /*
 
- $Id: mapping.cc,v 1.3 2007/04/27 06:01:49 garrett Exp $
+ $Id: mapping.cc,v 1.4 2009/04/28 21:12:42 rhuey Exp $
 
  AutoDock 
 
@@ -40,24 +40,32 @@
 extern FILE *logFile;
 
 //  This should be made more efficient.  As it is now, we (de facto) AlwaysEval!!!!
-Phenotype Individual::mapping(void)
+//Phenotype Individual::mapping(void)
+Individual &Individual::mapping(void)
 {
 
 #ifdef DEBUG
    (void)fprintf(logFile, "mapping.cc/Phenotype Individual::mapping(void)\n");
 #endif /* DEBUG */
-
+   //phenotyp.write sets phenotyp.evalflag to FALSE (0)
    phenotyp.write(*genotyp.vread(0), 0);
    phenotyp.write(*genotyp.vread(1), 1);
    phenotyp.write(*genotyp.vread(2), 2);
    phenotyp.write(*genotyp.vread(3), 3);
    phenotyp.write(*genotyp.vread(4), 4);
+   //Possible future improvement
+   //for (int i=0;i<5;i++) {
+   //   if (*phenotyp.vread(i)!=*genotyp.vread(i)) phenotyp.write(*genotyp.vread(i), i);
+   //}
+   
    value(Normal_Eval);
 
-   return(phenotyp);
+   return(*this);
+   //return(phenotyp);
 }
 
-Genotype Individual::inverse_mapping(void)
+//Genotype Individual::inverse_mapping(void)
+Individual &Individual::inverse_mapping(void)
 {
 
 #ifdef DEBUG
@@ -70,5 +78,6 @@ Genotype Individual::inverse_mapping(void)
    genotyp.write(*phenotyp.vread(3), 3);
    genotyp.write(*phenotyp.vread(4), 4);
 
-   return(genotyp);
+   return(*this);
+   //return(genotyp);
 }
