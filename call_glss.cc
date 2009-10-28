af548309030417caf992774d679d16b760ff5c47
Ruth Huey <rhuey@scripps.edu>
2009-10-28 17:34:23 +0000

    commented out initial call to thisPop.msort(1) line 380 so tests would pass
    CVS-ID: call_glss.cc 1.42

--- call_glss.cc
+++ call_glss.cc
@@ -1,6 +1,6 @@
 /*
 
- $Id: call_glss.cc,v 1.41 2009/10/23 22:49:46 mp Exp $
+ $Id: call_glss.cc,v 1.42 2009/10/28 17:34:23 rhuey Exp $
 
  AutoDock  
 
@@ -377,7 +377,7 @@ State call_glss(Global_Search *global_method, Local_Search *local_method,
 
     (void)fprintf( logFile, "Beginning Lamarckian Genetic Algorithm (LGA), with a maximum of %u\nenergy evaluations.\n\n", num_evals);
 
-    thisPop.msort(1);
+    //thisPop.msort(1);
     (void)fprintf(logFile,"Initial-Value: %.3f\n", thisPop[0].value(Normal_Eval));
 
     do {
