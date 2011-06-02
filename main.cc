606a96dff5d9eeae565bc6045de065a3c9880925
Michael Pique <mp@scripps.edu>
2011-06-02 22:28:31 +0000

    Corrected typo in commented out lines
    CVS-ID: main.cc 1.145

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 /* AutoDock
- $Id: main.cc,v 1.144 2011/06/02 22:26:17 mp Exp $
+ $Id: main.cc,v 1.145 2011/06/02 22:28:31 mp Exp $
 
 **  Function: Performs Automated Docking of Small Molecule into Macromolecule
 **Copyright (C) 2009 The Scripps Research Institute. All rights reserved.
@@ -111,7 +111,7 @@ extern Linear_FE_Model AD4;
 extern Real nb_group_energy[3]; ///< total energy of each nonbond group (intra-ligand, inter, and intra-receptor)
 extern int Nnb_array[3];  ///< number of nonbonds in the ligand, intermolecular and receptor groups
 
-static const char* const ident[] = {ident[1], "@(#)$Id: main.cc,v 1.144 2011/06/02 22:26:17 mp Exp $"};
+static const char* const ident[] = {ident[1], "@(#)$Id: main.cc,v 1.145 2011/06/02 22:28:31 mp Exp $"};
 
 
 int sel_prop_count = 0;
@@ -725,7 +725,7 @@ while( fgets(line, LINE_LEN, parFile) != NULL ) { /* PARSING-DPF parFile */
 
 banner( version_num.c_str() );
 
-(void) fprintf(logFile, "                           $Revision: 1.144 $\n\n");
+(void) fprintf(logFile, "                           $Revision: 1.145 $\n\n");
 (void) fprintf(logFile, "                   Compiled on %s at %s\n\n\n", __DATE__, __TIME__);
 
 
@@ -1965,9 +1965,9 @@ while( fgets(line, LINE_LEN, parFile) != NULL ) { /* PARSING-DPF parFile */
             if (dpf_keyword == DPF_QUATERNION0) {
                 pr( logFile, "Initial quaternion,  (x,y,z,w) =\t( %.3f, %.3f, %.3f, %.3f ),\n", sInit.Q.x, sInit.Q.y, sInit.Q.z, sInit.Q.w);
             } else {
-                \\if (dpf_keyword == DPF_QUAT0 && B_RandomQuat0)  {
-                    \\pr( logFile, "WARNING quat0 command is obsolete. Now use quaternion0 instead\n");
-                \\}
+                //if (dpf_keyword == DPF_QUAT0 && B_RandomQuat0)  {
+                    //pr( logFile, "WARNING quat0 command is obsolete. Now use quaternion0 instead\n");
+                //}
                 if (dpf_keyword == DPF_QUAT0 && !B_RandomQuat0)  {
                     pr( logFile, "WARNING quat0 command is obsolete. Now use quaternion0 or axisangle0 instead\n");
                 }
