0e5f8b28d0727715e699883cf66ab2eb6bba6e70
Michael Pique <mp@scripps.edu>
2012-01-25 02:22:14 +0000

    Rearranged logic so desolvation is included even for pairs with r>nbc; added e_vdW_Hb temporary so it need not be recomputed (differently) within eintcalprint block.
    CVS-ID: eintcal.cc 1.26

--- eintcal.cc
+++ eintcal.cc
@@ -1,6 +1,6 @@
 /*
 
- $Id: eintcal.cc,v 1.25 2011/10/03 17:30:20 rhuey Exp $
+ $Id: eintcal.cc,v 1.26 2012/01/25 02:22:14 mp Exp $
 
  AutoDock  
 
@@ -106,48 +106,23 @@ Real eintcalPrint( const NonbondParam * const nonbondlist,
   // if r is less than the non-bond-cutoff, 
   //  -OR-
   // If we are computing the unbound conformation then we ignore the non bond cutoff, NBC
-#ifndef  EINTCALPRINT //  eintcal [
-#   ifndef  NOSQRT
-        register double r=0.0L; //  SQRT
         // if we have defined USE_8A_CUTOFF, then NBC = 8
-        const double nbc  = B_use_non_bond_cutoff ? NBC  : 999;
-#   else
-        // if we have defined USE_8A_CUTOFF, then NBC = 8 // Xcode-gmm
         const double nbc2 = B_use_non_bond_cutoff ? NBC2 : 999 * 999;
-#   endif
-    //  eintcal ]
-#else // eintcalPrint [
-#   ifndef  NOSQRT
-        register double d=0.0L; //  SQRT 
-        //const double nbc  = NBC;
-        // if we have defined USE_8A_CUTOFF, then NBC = 8
-        const double nbc  = B_use_non_bond_cutoff ? NBC  : 999;
-#   else
-        //const double nbc2 = NBC2;
-        // if we have defined USE_8A_CUTOFF, then NBC = 8 // Xcode-gmm
-        const double nbc2 = B_use_non_bond_cutoff ? NBC2 : 999 * 999;
-#   endif
-#endif // eintcalPrint ]
 
-    register double dx=0.0L, dy=0.0L, dz=0.0L;
-    register double r2=0.0L;
+    double dx, dy, dz;
+    register double r2;
 
-    register double total_e_internal=0.0L; // total_e_internal = eint
+    double total_e_internal=0.0L; // total_e_internal = eint
 
-    register double e_elec=0.0L;
+    double e_elec=0.0L;
 
 #ifdef EINTCALPRINT
     double total_e_elec=0.0L;
     double total_e_vdW_Hb=0.0L;
-    double e_vdW_Hb=0.0L;
     double total_e_desolv=0.0L;
 #endif
 
     register int inb=0;
-    register int a1=0, a2=0;
-    register int t1=0, t2=0; // Xcode-gmm
-    register int nonbond_type=0; // if = 4, it is a 1_4;  otherwise it is another kind of nonbond
-    register int index_lt_NDIEL=0;
     register int nb_group=0;
     int inb_from=0;
     int inb_to=0;
@@ -196,16 +171,18 @@ Real eintcalPrint( const NonbondParam * const nonbondlist,
         // Loop over the non-bonds in this nonbond "group", "inb",
         for (inb = inb_from;  inb < inb_to;  inb++) {
 
-            double e_internal=0;  // e_internal = epair
-            double e_desolv=0;    // e_desolv = dpair
+	    int a1, a2;
+	    int index_lt_NDIEL=0;
 
+            double e_internal=0;  // e_internal = epair
+            double e_desolv;    // e_desolv = dpair
+	    double e_vdW_Hb=0;
+
+	    int nonbond_type; // if = 4, it is a 1_4;  otherwise it is another kind of nonbond
             a1 = nonbondlist[inb].a1;
             a2 = nonbondlist[inb].a2;
-            t1 = nonbondlist[inb].t1; // Xcode-gmm  // t1 is a map_index
-            t2 = nonbondlist[inb].t2; // Xcode-gmm  // t2 is a map_index
-            nonbond_type = nonbondlist[inb].nonbond_type;
+	    nonbond_type = nonbondlist[inb].nonbond_type;
             double nb_desolv = nonbondlist[inb].desolv;
-            double q1q2 = nonbondlist[inb].q1q2;
 
             dx = tcoord[a1][X] - tcoord[a2][X];
             dy = tcoord[a1][Y] - tcoord[a2][Y];
@@ -216,21 +193,18 @@ Real eintcalPrint( const NonbondParam * const nonbondlist,
             //| desolvation energy = sol_fn[dist] * ( rec.vol * (lig.solpar + qsolpar * |lig.charge|)
             //|                                     + lig.vol * (rec.solpar + qsolpar * |rec.charge|) );
             //|
+
+            r2 = sqhypotenuse(dx,dy,dz); // r2, the square of the separation between the atoms a1 and a2 in this non-bond, inb, 
+            r2 = clamp(r2, (RMIN_ELEC*RMIN_ELEC));
+
+	    // convert real-valued distance to an index for energy lookup tables
 #ifndef NOSQRT 
             // Use square-root, slower...
-
-            // r = the separation between the atoms a1 and a2 in this non-bond, inb, 
-            r = clamp(hypotenuse(dx,dy,dz), RMIN_ELEC); // clamp prevents electrostatic potential becoming too high when shorter than RMIN_ELEC
-            r2 = r*r;
-            register const int index = Ang_to_index(r); // convert real-valued distance r to an index for energy lookup tables
-
+            const int index = Ang_to_index(sqrt(r2)); 
 #else   
             //  Non-square-rooting version, faster...
-            r2 = sqhypotenuse(dx,dy,dz); // r2, the square of the separation between the atoms a1 and a2 in this non-bond, inb, 
-            r2 = clamp(r2, RMIN_ELEC2);
-            register const int index = SqAng_to_index(r2);
-
-#endif  // NOSQRT ]
+            const int index = SqAng_to_index(r2);
+#endif  // NOSQRT
 
             index_lt_NDIEL = BoundedNdiel(index);  // guarantees that index_lt_NDIEL is never greater than (NDIEL - 1)
 
@@ -239,32 +213,36 @@ Real eintcalPrint( const NonbondParam * const nonbondlist,
             if (B_calcIntElec) {
                 //  Calculate  Electrostatic  Energy
                 double r_dielectric = ptr_ad_energy_tables->r_epsilon_fn[index_lt_NDIEL];
-                e_elec = q1q2 * r_dielectric;
+                e_elec = nonbondlist[inb].q1q2 * r_dielectric;
                 e_internal = e_elec;
             }
 
-            //if  ( r2 < nbc2 ) {   
-            int index_lt_NEINT = BoundedNeint(index);  // guarantees that index_lt_NEINT is never greater than (NEINT - 1) (scaled NBC, non-bond cutoff)
-            if (B_include_1_4_interactions != 0 && nonbond_type == 4) {
-                //| Compute a scaled 1-4 interaction,
-                e_internal += scale_1_4 * (ptr_ad_energy_tables->e_vdW_Hb[index_lt_NEINT][t2][t1] + e_desolv);
-            } else {
-                e_internal += ptr_ad_energy_tables->e_vdW_Hb[index_lt_NEINT][t2][t1] + e_desolv;
-            }
-            //}
-
+            if  ( r2 < nbc2 ) {   
+		int t1, t2; 
+		t1 = nonbondlist[inb].t1; // t1 is a map_index
+		t2 = nonbondlist[inb].t2; // t2 is a map_index
+		 int index_lt_NEINT = BoundedNeint(index);  // guarantees that index_lt_NEINT is never greater than (NEINT - 1) (scaled NBC, non-bond cutoff)
+		e_vdW_Hb= ptr_ad_energy_tables->e_vdW_Hb[index_lt_NEINT][t2][t1];
+                if (B_include_1_4_interactions && nonbond_type==4 ) {
+                    //| Compute a scaled 1-4 interaction,
+                    e_internal += scale_1_4 * (e_vdW_Hb + e_desolv);
+                } else {
+                    e_internal += e_vdW_Hb + e_desolv;
+                }
+	   }
+	   else e_internal += e_desolv; // no NBC-based cutoff for desolvation
 
 
           total_e_internal += e_internal;
 #ifdef EINTCALPRINT // eintcalPrint [
 
+	  // MP TODO 2012 are these correct for 1_4 terms too?
           total_e_desolv   += e_desolv;
           total_e_elec     += e_elec;
           double dielectric = ptr_ad_energy_tables->epsilon_fn[index_lt_NDIEL];
 
           if (B_calcIntElec) {
 
-              e_vdW_Hb = e_internal - e_desolv - e_elec,
               pr( logFile, " %6d   %5d-%-5d  %7.2lf  %+8.3lf  %+8.3lf  %+8.3lf  %+8.3lf   %+8.3lf   %d  %8.3lf\n", 
                     (int)(inb+1), (int)(a1+1), (int)(a2+1), (double)sqrt(r2), 
                     (double)e_internal, (double)e_elec, (double)e_vdW_Hb, (double)e_desolv, 
@@ -272,7 +250,6 @@ Real eintcalPrint( const NonbondParam * const nonbondlist,
                  );
           } else {
 
-              e_vdW_Hb = e_internal - e_desolv,
               pr( logFile, " %6d   %5d-%-5d  %7.2lf  %+8.3lf  %+8.3lf  %+8.3lf   %+8.3lf   %d  %8.3lf\n", 
                     (int)(inb+1), (int)(a1+1), (int)(a2+1), (double)sqrt(r2), 
                     (double)e_internal, (double)e_vdW_Hb, (double)e_desolv, 
